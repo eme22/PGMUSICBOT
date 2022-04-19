a1eb1803fb8891214de2f941d08975d709c84f76
eme22 <48258734+eme22@users.noreply.github.com>
2022-04-19 14:01:32 -0500

    DiscImage

---
+++ src/main/java/com/eme22/bolo/commands/CagarCmd.java
@@ -0,0 +1,18 @@
+package com.eme22.bolo.commands;
+
+import com.jagrosh.jdautilities.command.CommandEvent;
+import com.jagrosh.jdautilities.command.SlashCommand;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+
+public class CagarCmd extends SlashCommand {
+
+
+    @Override
+    protected void execute(SlashCommandEvent event) {
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        super.execute(event);
+    }
+}
